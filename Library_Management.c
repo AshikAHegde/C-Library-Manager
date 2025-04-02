0d619dc93f3789a4e1f4b69c4b2835721510473e
AshikAHegde <hegdeashik12@gmail.com>
2025-04-02 22:47:46 +0530

    Made Book and IssueBook Structure

--- Library_Management.c
+++ Library_Management.c
@@ -8,3 +8,21 @@
 #define MAX_NAME_LEN 50
 #define MAX_TITLE_LEN 100
 #define MAX_AUTHOR_LEN 100
+
+struct Book
+{
+	int sr_no1;
+	int id;
+	char title[MAX_TITLE_LEN];
+	char author[MAX_AUTHOR_LEN];
+	int quantity;
+	int allocatedCount;
+};
+
+struct IssuedBook
+{
+	int sr_no2, id;
+	char userName[MAX_NAME_LEN];
+	int issueDate;
+	int returnDate;
+};
