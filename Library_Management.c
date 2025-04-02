5556ce687a8aefd60544b7f6041342aceca7e7f6
AshikAHegde <hegdeashik12@gmail.com>
2025-04-02 22:46:45 +0530

    Include All Constants

--- Library_Management.c
+++ Library_Management.c
@@ -2,3 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
+#define MAX_BOOKS 100
+#define MAX_ISSUED 100
+#define MAX_NAME_LEN 50
+#define MAX_TITLE_LEN 100
+#define MAX_AUTHOR_LEN 100
